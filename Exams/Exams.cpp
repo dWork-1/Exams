5ee354cc510364720b773925b04598c9d48e4f5f
tsardd8@gmail.com <tsardd8@gmail.com>
2022-05-30 13:47:06 +0300

    refuctor class`

--- Exams/Exams.cpp
+++ Exams/Exams.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Student.h"
-#include "get.h"
+#include "user.h"
 #include<string>
 using namespace std;
 
@@ -11,13 +11,9 @@ int main()
     string id = "00000000";
     string exam = "00000000";
     student abitur();
+    user at(name, id);
+    cout << at.get_name() << endl;
 
-    getter inf("marks.txt");
-    int mark = 0;//inf.get_mark(id, exam);
-    cout << mark<< endl;
-
-
-    inf.close_stream();
     system("pause");
     return EXIT_SUCCESS;
 }
