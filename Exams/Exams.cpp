fd99fca39c87ebfba93b8c470bb3961aad848a95
tsardd8@gmail.com <tsardd8@gmail.com>
2022-05-30 14:00:16 +0300

    create users_test for testing

--- Exams/Exams.cpp
+++ Exams/Exams.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "Student.h"
 #include "user.h"
 #include<string>
 using namespace std;
@@ -10,9 +9,12 @@ int main()
     string name = "Vasya";
     string id = "00000000";
     string exam = "00000000";
-    student abitur();
-    user at(name, id);
-    cout << at.get_name() << endl;
+
+    user abi()
+
+    //student abitur();
+    //user at(name, id);
+    //cout << at.get_name() << endl;
 
     system("pause");
     return EXIT_SUCCESS;
