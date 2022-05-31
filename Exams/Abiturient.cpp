36e869b3696dea66b28483dfadd3d1a8571bfab8
Reckless Lime <valentinkabovkuns@gmail.com>
2022-05-31 18:42:02 +0300

    чтение и запись файла

--- Exams/Abiturient.cpp
+++
@@ -1,1 +0,0 @@
-#include "Abiturient.h"
--- Exams/Exams.cpp
+++ Exams/Exams.cpp
@@ -1,128 +1,124 @@
 // Exams.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-#include "Rating.h"
-#define _USE_MATH_DEFINES
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <string>
-#include <sstream>
+#include<iostream>
+#include<string> 
+#include<fstream>
 
 using namespace std;
+string user; bool a = 0;
+int menu();
 
-void clear(const string filename);
-void get_data_1(const string filename, int& sz, bool& u, int& y);
-
-void Abiturient::show_f(string& s_z) // запись данных в строку
+void write_file(string* path1, string value)
 {
-    s_z = get_surname() + " " + get_name() + " " + get_patronymic();
-}
-
-void Abiturient::show_1(const string filename, const string s_z) // запись данных в файл
+    fstream f1;
+    {
+        f1.open(*path1, fstream::in | fstream::out);
+        f1 << value << "\n";
+    }
+    f1.close();
+};
+void write_file(string* path1, string value, string value2)
 {
-    ofstream out(filename, ios::app);
-    out << s_z << endl;
-    out.close();
-}
-
-void Rating::task_1(const int sz, const string filename, string& s_z)
+    fstream f1;
+    {
+        f1.open(*path1, fstream::in | fstream::out | fstream::app);
+        f1 << value << "\n";
+    }
+    f1.close();
+};
+void read_file(string* path1)
 {
-    string user;
+    fstream f1;
+    {
+        f1.open(*path1, fstream::in);
+        while (!f1.eof())
+        {
+            string str = "";
+            getline(f1, str);
+            if (str == user)
+            {
+                cout << "Вход подтвержден." << endl;
+                cout << "Добро пожаловать, " << str << "." << endl;
+                break;
+            }
+            else if (str != user && !f1.eof())
+            {
+                continue;
+            }
+            else
+            {
+                cerr << "Пользователь не найден." << endl;
+                cout << "Хотите добавить нового пользователя?" << endl << "\t1. Да or 0. Нет." << endl;
+                cout << "Ваш выбор: ";
+                cin >> a;
+                if (a == 1)
+                {
+                    system("cls");
+                    menu();
+                }
+                break;
+            }
+        }
+    }
+    f1.close();
+};
+class Out_to_screen {
+public:
+    string path; string path1;
+    Out_to_screen()
+    {
+        cout << "erorr not path" << endl;
+    }
+    Out_to_screen(string path)
+    {
+        path1 = path;
+        fstream f;
+        {
+            f.open(path, fstream::in | fstream::out);
+            if (f.is_open())
+            {
+
+            }
+        }
+        f.close();;
+    }
+    void write_new_user(string value)
+    {
+        write_file(&path1, value);
+    }
+    void write_new_user(string value, string value2)
+    {
+        if (value2 == "a")
+        {
+            fstream f2;
+            write_file(&path1, value, value2);
+        }
+        else
+        {
+            cout << "Error." << endl;
+        }
+    }
+    void read()
+    {
+        read_file(&path1);
+    }
+};
+int main() //главное меню (пользователь ( + ср.балл + предмет + прошел \ не прошел))
+{
+    setlocale(LC_ALL, "ru");
+    cout << " Login:" << endl;
     cin >> user;
-    if (user == s_z)
-    {
-        cout << " Вход успешно выполнен." << endl << endl;
-        cout << " Добро пожаловать, " << user << ".\n";
-    }
-}
-
-void get_data_1(const string filename, int& sz, bool& u, int& y) // подсчет кол-ва строк в файле
-{
-    char c;
-    y = 0;
-    u = false;
-    ifstream file_in(filename);
-    if (file_in)
-    {
-        y = 1;
-        while (!file_in.eof())
-        {
-            if ((c = file_in.get()) == '\n' || c == 'EOF')
-            {
-                sz++;
-                u = true;
-            }
-        }
-    }
-    else
-        u = false;
-
-    file_in.close();
-    cout << endl;
-}
-
-
-void clear(const string filename) // очистка файла
-{
-    ofstream file_on(filename);
-    file_on.open(filename, ofstream::trunc);
-    file_on.close();
-}
-
-int menu() {
-    system("cls");
-    cout << "Добро пожаловать!" << endl;
-    cout << "Выберите элемент меню: " << endl;
-    cout << "\t1. Зайти в существующий кабинет." << endl;
-    cout << "\t2. Добавить нового абитуриента." << endl;
-    cout << "\t0 для выхода" << endl;
-    cout << "Ваш выбор: ";
-    int a = 0;
-    cin >> a;
-    system("cls");
-    return a;
-}
-
-int main() {
-
-    setlocale(LC_ALL, "");
-
-    string file = "Файл для хранения данных.txt";
-    string username;
-
-    bool u = false;
-    int sz = 0, y = 0, value = 0;
-    string str;
-
-    //cout << "Введите название файла из которого будет чтение:" << endl;
-    //cin >> file;
-    get_data_1(file, sz, u, y);
-    Rating abiturient(sz);
-
-    unsigned int a = 0;
-    do {
-        a = menu();
-        switch (a)
-        {
-        case 1:
-            cout << " Введите Ваш логин: ";
-            // проверка наличия файла
-            if ((u == true) && (y == 1))
-            {
-                abiturient.get_data(file, sz);
-                abiturient.task_1(sz, file, str); // проверка на наличие абитуриента в файле
-                abiturient.~Rating();
-            }
-            else if ((y == 1) && (u == false))
-            {
-                cout << "Ошибка программы..." << endl;
-                return 0;
-            }
-
-            system("pause");
-            break;
-        }
-
-    } while (a);
+    Out_to_screen f("База.txt");
+    f.read();
+    return 0;
     system("pause");
-    return EXIT_SUCCESS;
+}
+
+int menu()
+{
+    cout << " New login:" << endl;
+    cin >> user;
+    Out_to_screen f("Base.txt");
+    f.write_new_user(user, "a");
+    f.read();
+    return 0;
 }
